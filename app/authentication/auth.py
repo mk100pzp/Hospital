4ef97934099d06f9c01741d8071880a3806c5d18
shayanpzp <shayanpzp1@gmail.com>
2023-10-31 20:21:33 +0330

    https://s6.uupload.ir/files/mysql_demo_physical_data_model-2023-10-31_19-56_q2og.png chore : changing db.py chore : changing auth.py

--- app/authentication/auth.py
+++ app/authentication/auth.py
@@ -1,2 +1,39 @@
+import bcrypt
+
+class Authentication:
+    def __init__(self):
+        pass
+
+
+    def hash_password(self, password):
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
+
+
+    def check_password(self, hashed_password, input_password):
+        return bcrypt.checkpw(input_password.encode('utf-8'), hashed_password)
+
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+
+
+user = Authentication()
+
+
+password = "password123"
+hashed_password = user.hash_password(password)
+
+
+
+input_password = "password000"
+if user.check_password(hashed_password, input_password):
+    print("Password is correct :)")
+else:
+    print("Password is incorrect ! :(")
+
 
 
--- app/database/db.py
+++ app/database/db.py
@@ -1,1 +1,37 @@
+import psycopg2
 
+class Database:
+
+    def __init__(self, dbname, user, password, host, port):
+
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+
+        self.connection = None
+        self.cursor = None
+
+
+    def connect(self):
+
+        try:
+            self.connection = psycopg2.connect(
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port
+            )
+
+            self.cursor = self.connection.cursor()
+
+        except psycopg2.Error as e:
+            print(f"Error : {e}")
+
+
+    def close_connection(self):
+        if self.connection:
+            self.cursor.close()
+            self.connection.close()
