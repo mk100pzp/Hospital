37e1c0db9523086a1bfe3374e0562ace624c97a9
shayanpzp <shayanpzp1@gmail.com>
2023-11-01 12:43:00 +0330

    chore : write all class(models) of database tabels

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,67 @@
+class User:
+    def __init__(self, user_id, user_name, user_pass, user_email, user_mobile, role):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.user_pass = user_pass
+        self.user_email = user_email
+        self.user_mobile = user_mobile
+        self.role = role
+
+
 class Admin:
     def __init__(self, admin_id, users_user_id):
         self.admin_id = admin_id
         self.users_user_id = users_user_id
+
+
+class Doctor:
+    def __init__(self, doctor_id, experties, work_experienceit, salary, address, visit_price, users_user_id):
+        self.doctor_id = doctor_id
+        self.experties = experties
+        self.work_experienceit = work_experienceit
+        self.salary = salary
+        self.address = address
+        self.visit_price = visit_price
+        self.users_user_id = users_user_id
+
+
+class Paient:
+    def __init__(self, paient_id, paient_name, paient_address, users_user_id):
+        self.paient_id = paient_id
+        self.paient_name = paient_name
+        self.paient_address = paient_address
+        self.users_user_id = users_user_id
+
+
+class Visit_From:
+    def __init__(self, form_id, doctor_doctor_id, paient_paient_id, form_name, visit_date, visit_desc, hospitalization, duration_of_hospitalization):
+        self.form_id = form_id
+        self.doctor_doctor_id = doctor_doctor_id
+        self.paient_paient_id = paient_paient_id
+        self.form_name = form_name
+        self.visit_date = visit_date
+        self.visit_desc = visit_desc
+        self.hospitalization = hospitalization
+        self.duration_of_hospitalization = duration_of_hospitalization
+
+
+class Paient_Bill:
+    def __init__(self, bill_id, paient_paient_id, date, total_amount, paient_share, amount_paid, the_remaining_amount, insurance_contribution):
+        self.bill_id = bill_id
+        self.paient_paient_id = paient_paient_id
+        self.date = date
+        self.total_amount = total_amount
+        self.paient_share = paient_share
+        self.amount_paid = amount_paid
+        self.the_remaining_amount = the_remaining_amount
+        self.insurance_contribution = insurance_contribution
+
+
+class Medical_Record:
+    def __init__(self, record_id, paient_id, doctor_id, date_of_visit, doctor_doctor_id, paient_paient_id):
+        self.record_id = record_id
+        self.paient_id = paient_id
+        self.doctor_id = doctor_id
+        self.date_of_visit = date_of_visit
+        self.doctor_doctor_id = doctor_doctor_id
+        self.paient_paient_id = paient_paient_id
