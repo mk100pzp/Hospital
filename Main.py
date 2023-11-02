0bab00b9da0c19432c55dfbb82804aef727ff331
fariba_r <faribarezaee321@gmail.com>
2023-11-02 09:57:52 +0330

    chor: update main and menu file

---
+++ Main.py
@@ -0,0 +1,120 @@
+import pro1_exception
+import new_menue
+
+class Menue:
+    list_obj=[]
+
+    def __init__(self,name,parent):
+        self.name = name
+        self.parent = parent
+        Menue.list_obj.append(self)
+
+    # def __repr__(self) -> str:
+    #     return self.name
+
+    # def __str__(self) -> str:
+    #     return f"{self.name},{self.parent}"
+    
+    @classmethod
+    def find_obj(cls,name,parent_name):
+        for obj in cls.list_obj:
+            
+            if obj.name == name and obj.parent.name==parent_name:
+                return obj
+            
+   
+    @staticmethod
+    def print(enumerate_item):
+        
+
+                
+                    enumerate_item=enumerate(enumerate_item)
+                    for i , item in enumerate_item:
+
+                        print(f"{str(i+1)}:{item['name']}")
+    @staticmethod
+    def match_number_to_name(enumerate_item,number):
+        
+        return enumerate_item[number]["name"]
+    
+    @staticmethod
+    def input():
+        number_choise=input("Number")
+        return number_choise
+
+
+
+class SubMenu(Menue):
+    def __init__(self,name,parent,child_list):
+        super().__init__(name,parent)
+        self.child_list =child_list
+
+   
+    
+
+class ActionMenu(Menue):
+
+    def __init__(self,name,parent,list_action_func):
+         super().__init__(name,parent)
+         self.list_action_func=list_action_func
+
+
+
+def generate_obj(menue,parent=None):
+   
+
+
+    for item in menue:
+
+
+        if "action" in item:
+            obj=ActionMenu(item["name"],parent,item["action"])
+           
+        
+        elif "children" in item:
+            
+            obj=SubMenu(item["name"],parent,item["children"])
+           
+           
+            generate_obj(item["children"],obj)
+
+
+def display(obj):
+    try:
+        
+            
+            if 'function' in obj.child_list[0]:
+                
+                if not  obj.child_list[0]['function'][0]():
+                     
+                     display(obj.parent)
+                     
+               
+                 
+        
+            Menue.print(obj.child_list)
+            number_choise=Menue.input()
+        
+            name_choise=Menue.match_number_to_name(obj.child_list,int(number_choise)-1)
+            
+            new_obj=Menue.find_obj(name_choise,obj.name)
+        
+            display(new_obj)
+    except AttributeError:
+            
+            for func in obj.list_action_func:
+                func()
+    except (ValueError,IndexError):
+            if number_choise=="b":
+                if obj.parent==None:
+                    print( "end")
+                else:
+                    display(obj.parent)
+            else:
+                    pro1_exception.Exception.key_error()
+                    display(obj)
+
+
+
+generate_obj(new_menue.enter)
+display(Menue.list_obj[0])
---
+++ app/menu/menu.py
@@ -0,0 +1,144 @@
+import pro1_operator
+enter=[{
+"name":"main",
+"children":[{
+        "name":"registration",
+        "children":[
+                        {
+                            "name":"doctor",
+                            "action":[pro1_operator.doctor_registration],
+                            
+
+                        },
+                        {
+                            "name":"patient",
+                            "action":[pro1_operator.patient_registeration],
+                            
+                        },
+                        
+                    ]
+    },
+
+
+
+  
+    {
+        "name":"log in",
+        "children":[
+                        {
+                            "name":"doctor",
+                           
+                           "children":[{
+                                        "name":"add visit time",
+                                        "function":[pro1_operator.login_doctor],
+                                        "action":[pro1_operator.add_visit_time]
+                                        },
+
+
+
+                                    
+                                        {
+                                        "name":"edit visit time",
+                                        "function":[pro1_operator.login_doctor],
+                                        "action":[pro1_operator.edit_visit_time]
+                                        },
+                                        {
+                                        "name":"remove visit time",
+                                        "function":[pro1_operator.login_doctor],
+                                        "action":[pro1_operator.remove_visit_time]
+                                        },
+
+                            ]
+
+                        },
+                        {
+                            "name":"patient",
+                            
+                            "children":[{
+                                    "function":[pro1_operator.login_patient],
+                                    "name":"get visit time",
+                                    "action":[pro1_operator.get_visit_time]
+                                    },
+
+
+
+                                
+                                    {"function":[pro1_operator.login_patient],
+                                    "name":"cancel visit time",
+                                    "action":[pro1_operator.cancel_visit_time]
+                                    },
+                                
+                                    {"function":[pro1_operator.login_patient],
+                                    "name":"show catched visit time",
+                                    "action":[pro1_operator.catched_visit_time]
+                                    },
+
+                                    {"function":[pro1_operator.login_patient],
+                                    "name":"show bill",
+                                    "action":[pro1_operator.show_bill]
+                                    },
+
+                                    {"function":[pro1_operator.login_patient],
+                                    "name":"show visit form",
+                                    "action":[pro1_operator.show_visit_form]
+                                    },
+
+                            ]
+
+                        },
+                        {
+                            "name":"admin",
+                            "children":[
+                                {
+                                        "function":[pro1_operator.login_admin],
+                                        "name":"add new admin",
+                                    "action":[pro1_operator.add_new_admin]
+                                    },
+
+
+
+                                
+                                    {
+                                        "function":[pro1_operator.login_admin],
+                                        "name":"show patient information",
+                                    "action":[pro1_operator.show_patient_information]
+                                    },
+
+                                    {
+                                        "function":[pro1_operator.login_admin],
+                                        "name":"show doctor information",
+                                    "action":[pro1_operator.show_doctor_information]
+                                    },
+
+                                    {  "function":[pro1_operator.login_admin],
+                                        "name":"income of visit",
+                                    "action":[pro1_operator.income_visit]
+                                    },
+
+                                    {
+                                        "function":[pro1_operator.login_admin],
+                                        "name":"show number of patient's visits",
+                                    "action":[pro1_operator.number_visits]
+                                    },
+
+                                    {   "function":[pro1_operator.login_admin],
+                                        "name":"income of hospital",
+                                    "action":[pro1_operator.income_hospital]
+                                    },
+
+                                                            ]
+                                                        
+                                                        }
+                                        ]
+                                                        
+                                    }
+
+]}
+]
+
+
+"main:
+    1-restration
+                a)doctor
+                b)patient
+    2-log in"
