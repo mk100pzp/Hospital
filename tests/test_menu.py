1977bbb5fe5e77a65bc82dc7584434c3aee8a04b
mdshady <moeinahouei@gmail.com>
2023-11-06 02:26:13 +0330

    test_menue based on existance of optopns and varailbles -ep2 : addressing changed

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,27 +1,27 @@
 import unittest
 from app.authentication import auth
 from app.models import models
-from app.menu import enter
+from app.menu import menu
 
 
 class TestMenu(unittest.TestCase):
     def test_doctor_registration_action(self):
-        self.assertIn(auth.Authentication.doctor_registration, enter[0]['children'][0]['children'][0]['action'])
+        self.assertIn(auth.Authentication.doctor_registration, menu.enter[0]['children'][0]['children'][0]['action'])
 
     def test_patient_registration_action(self):
-        self.assertIn(auth.Authentication.patient_registration, enter[0]['children'][0]['children'][1]['action'])
+        self.assertIn(auth.Authentication.patient_registration, menu.enter[0]['children'][0]['children'][1]['action'])
 
     def test_add_visit_time_function(self):
-        self.assertIn(auth.Authentication.login_doctor, enter[0]['children'][1]['children'][0]['function'])
-        self.assertIn(models.Visit_Date.create_visit_date, enter[0]['children'][1]['children'][0]['action'])
+        self.assertIn(auth.Authentication.login_doctor, menu.enter[0]['children'][1]['children'][0]['function'])
+        self.assertIn(models.Visit_Date.create_visit_date, menu.enter[0]['children'][1]['children'][0]['action'])
 
     def test_remove_visit_time_function(self):
-        self.assertIn(auth.Authentication.login_doctor, enter[0]['children'][1]['children'][1]['function'])
-        self.assertIn(models.Visit_Date.remove_visit_time, enter[0]['children'][1]['children'][1]['action'])
+        self.assertIn(auth.Authentication.login_doctor, menu.enter[0]['children'][1]['children'][1]['function'])
+        self.assertIn(models.Visit_Date.remove_visit_time, menu.enter[0]['children'][1]['children'][1]['action'])
 
     def test_get_visit_time_function(self):
-        self.assertIn(auth.Authentication.login_patient, enter[0]['children'][1]['children'][2]['function'])
-        self.assertIn(models.Paient.get_visit_time, enter[0]['children'][1]['children'][2]['action'])
+        self.assertIn(auth.Authentication.login_patient, menu.enter[0]['children'][1]['children'][2]['function'])
+        self.assertIn(models.Paient.get_visit_time, menu.enter[0]['children'][1]['children'][2]['action'])
 
     # Add more test cases for other functions and actions
 
