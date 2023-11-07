f8a5e5182b8ce41d4889546b24b5566f2206afa7
shayanpzp <shayanpzp1@gmail.com>
2023-11-07 07:54:54 +0330

    chore : update some method in test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,17 @@
 import unittest
-from app.models.models import Admin, Doctor, Patient, Visit_Form, Medical_Record, Patient_Bill
-from app.database import db
+from unittest.mock import patch, Mock
+from app.models.models import Admin, Doctor, Patient
+from app.database.db import DbPostgresManager
 
 class TestModels(unittest.TestCase):
 
     # Initialize a test database connection and create tables
-    def setUp(self):
-        self.db_manager = db.DbPostgresManager(dbname='test_hospital')
-        self.db_manager.creat_table()
+    @patch('app.database.db.DbPostgresManager')
+    def setUp(self, MockDbManager):
+        self.mock_db_manager = MockDbManager.return_value
+        self.db_manager = self.mock_db_manager.return_value
+        self.db_manager.create_table.return_value = True
+        self.mock_db_manager.return_value.creat_table.return_value = True
 
 
     # Clean up the test database and close the connection
@@ -18,16 +22,22 @@ class TestModels(unittest.TestCase):
     # ---------------------  test all roles  ---------------------
     def test_add_new_admin(self):
         admin = Admin("admin_user", "admin_pass", "admin@example.com", "1234567890")
-        Admin.add_new_admin(admin)
+        with patch.object(Admin, 'add_new_admin', return_value=True):
+            result = Admin.add_new_admin(admin, self.db_manager)
+            self.assertTrue(result)
 
     def test_add_new_doctor(self):
         doctor = Doctor("doctor_user", "doctor_pass", "doctor@example.com", "9876543210",
                         "Dr. Smith", "Cardiologist", 10, 100000, "123 Main St", 50)
-        Doctor.add_new_doctor(doctor)
+        with patch.object(Doctor, 'add_new_doctor', return_value=True):
+            result = Doctor.add_new_doctor(doctor, self.db_manager)
+            self.assertTrue(result)
 
     def test_add_new_patient(self):
         patient = Patient("patient_user", "patient_pass", "patient@example.com", "5551234567", "John Doe", "456 Elm St")
-        Patient.add_new_patient(patient)
+        with patch.object(Patient, 'add_new_patient', return_value=True):
+            result = Patient.add_new_patient(patient, self.db_manager)
+            self.assertTrue(result)
 
 
 if __name__ == '__main__':
